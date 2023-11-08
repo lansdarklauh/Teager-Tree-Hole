0ff9511963dc4112f3143e598ac410d4ed4ed241
lansdarklauh <68766437+lansdarklauh@users.noreply.github.com>
2023-11-08 20:42:53 +0800

    构建rn框架

---
+++ Project/ReactNativeApp/babel.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-env',
+    {
+      'targets': {
+        'chrome': '49',
+        'ios': '10',
+      },
+    }],
+};
